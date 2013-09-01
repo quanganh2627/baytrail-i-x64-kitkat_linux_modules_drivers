f719b5d0cf47d5b3e767d7cebb7e683142d44821
Wang Yiyang <yiyang.wang@intel.com>
2013-09-02 02:48:27 +0800

    HSU: support ANN platform hsu
    BZ: 134692
    
    add ANN platform hsu code.
    
    Change-Id: I1fb4ade35d092de6dab0357ba8e9f87081a8018d
    Signed-off-by: Wang Yiyang <yiyang.wang@intel.com>
    Reviewed-on: http://android.intel.com:8080/128920
    Reviewed-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: Du, Alek <alek.du@intel.com>
    Tested-by: Wang, Zhifeng <zhifeng.wang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- hsu/mfd.c
+++ hsu/mfd.c
@@ -929,7 +929,6 @@ static void hsu_dma_chan_handler(struct hsu_port *hsu, int index)
 static irqreturn_t hsu_port_irq(int irq, void *dev_id)
 {
 	struct uart_hsu_port *up = dev_id;
-	struct hsu_port_cfg *cfg = phsu->configs[up->index];
 	unsigned long flags;
 	u8 lsr;
 
@@ -2473,9 +2472,10 @@ static int serial_hsu_dma_probe(struct pci_dev *pdev,
 		dchan++;
 	}
 
-	/* TNG chip from B0 stepping */
-	if (intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER &&
-		pdev->revision >= 0x1) {
+	/* ANN all and TNG chip from B0 stepping */
+	if ((intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_TANGIER &&
+		pdev->revision >= 0x1) ||
+		intel_mid_identify_cpu() == INTEL_MID_CPU_CHIP_ANNIEDALE) {
 		phsu->irq_port_and_dma = 1;
 	} else {
 		phsu->dma_irq = pdev->irq;
