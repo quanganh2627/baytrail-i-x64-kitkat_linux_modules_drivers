090563a8b297e89ff04e195dd07e2928139e2e48
Albin B <albin.bala.krishnan@intel.com>
2013-10-02 19:51:17 +0530

    [EM/BCU]: Support for various camera flash throttle levels (camflash_ctrl sysfs)
    BZ: 142552
    
    Issue Description: The CMS will take proactive action to avoid the peak current
    condition based on the battery level by using camflash_ctrl sysfs interface. The
    existing camflash_ctrl sysfs interface supports only the Camera flash ON and OFF
    states by accepting the values 0 or 1 only. In this case camera flash intensity
    control/reduction cannot be done step by step using only these two values.
    
    Solution: To control the camera flash intensity step by step, the camflash_ctrl
    sysfs interface has to support the NORMAL, WARNING, ALERT and CRITICAL levels
    and the values from 0 to 3. So that CMS can write the value into the
    camflash_ctrl sysfs interface and camera HAL can take action accordingly to
    control the camera flash intensity.
    
    Category: device enablement
    Domain: EM.BCU-MERR
    Origin: internal
    Upstream-Candidate: no, proprietary
    
    Change-Id: Ia1d46515cf9c5ae42e7b58a8497c5ef70b843ad1
    Signed-off-by: Albin B <albin.bala.krishnan@intel.com>
    Reviewed-on: http://android.intel.com:8080/135850
    Reviewed-by: Jin, AlbertX <albertx.jin@intel.com>
    Tested-by: Jin, AlbertX <albertx.jin@intel.com>

--- hwmon/intel_mrfl_ocd.c
+++ hwmon/intel_mrfl_ocd.c
@@ -54,8 +54,8 @@
 
 #define DRIVER_NAME "bcove_bcu"
 
-#define CAMFLASH_STATE_ON	1
-#define CAMFLASH_STATE_OFF	0
+#define CAMFLASH_STATE_NORMAL	0
+#define CAMFLASH_STATE_CRITICAL	3
 
 /* 'enum' of BCU events */
 enum bcu_events { VWARN1, VWARN2, VCRIT, GSMPULSE, TXPWRTH, UNKNOWN, __COUNT };
@@ -400,7 +400,8 @@ static ssize_t store_camflash_ctrl(struct device *dev,
 	if (kstrtou8(buf, 10, &value))
 		return -EINVAL;
 
-	if ((value != CAMFLASH_STATE_ON) && (value != CAMFLASH_STATE_OFF))
+	if ((value < CAMFLASH_STATE_NORMAL) ||
+		(value > CAMFLASH_STATE_CRITICAL))
 		return -EINVAL;
 
 	cam_flash_state = value;
@@ -980,7 +981,7 @@ static int mrfl_ocd_probe(struct platform_device *pdev)
 
 	enable_volt_trip_points();
 	enable_current_trip_points();
-	cam_flash_state = CAMFLASH_STATE_ON;
+	cam_flash_state = CAMFLASH_STATE_NORMAL;
 
 	/* Initializing delayed work for re-enabling vwarn1 interrupt */
 	INIT_DELAYED_WORK(&cinfo->vwarn1_irq_work, vwarn1_irq_enable_work);
