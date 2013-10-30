d00b5e5de3277f51590cf0622fd847787d7b96cc
Teemu Piiroinen <teemux.piiroinen@intel.com>
2013-10-30 16:34:04 +0200

    Calibration interrupt mode support for r69001.
    BZ: 148032
    
    Change-Id: I724aa77c3419111a11e2a491d18e54ec16780908
    Signed-off-by: Teemu Piiroinen <teemux.piiroinen@intel.com>

--- input/touchscreen/r69001-ts-i2c.c
+++ input/touchscreen/r69001-ts-i2c.c
@@ -29,6 +29,14 @@
 #include <linux/gpio.h>
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_DEBUG_FS
+#include <linux/debugfs.h>
+
+#define MODE_COUNT                  4
+#define MODE_STRING_MAX_LEN         30
+#define MODE_FILE_NAME              "mode"
+#endif
+
 #define CONFIG_R69001_POLLING_TIME 10
 #include <linux/r69001-ts.h>
 
@@ -85,7 +93,11 @@
 #define INTERRUPT_MODE              R69001_TS_INTERRUPT_MODE
 #define POLLING_MODE                R69001_TS_POLLING_MODE
 #define POLLING_LOW_EDGE_MODE       R69001_TS_POLLING_LOW_EDGE_MODE
+#define CALIBRATION_INTERRUPT_MODE  R69001_TS_CALIBRATION_INTERRUPT_MODE
 #define UNKNOW_MODE                 255
+
+#define DEFAULT_INTERRUPT_MASK      0x0e
+#define CALIBRATION_INTERRUPT_MASK  0x08
 
 struct r69001_ts_finger {
 	u16 x;
@@ -109,6 +121,12 @@ struct r69001_ts_data {
 	u8 mode;
 	u8 t_num;
 };
+
+#ifdef CONFIG_DEBUG_FS
+static struct dentry *r69001_ts_dbgfs_root;
+static const char * const r69001_ts_modes[] = { "interrupt", "polling",
+		"polling low edge", "calibration interrupt" };
+#endif
 
 static void r69001_set_mode(struct r69001_ts_data *ts, u8 mode, u16 poll_time);
 
@@ -248,18 +266,21 @@ static irqreturn_t r69001_ts_irq_handler(int irq, void *dev_id)
 
 	r69001_ts_read_data(ts, REG_CONTROL, REG_SCAN_MODE, 1, &mode);
 
-	if (mode == SCAN_MODE_STOP) {
-		/* if receive a touchscreen interrupt, but the scan mode is stop
-		 * that means touch panel just power on, so re-init it
-		 */
-		ts->data.mode.mode = UNKNOW_MODE;
-		r69001_ts_write_data(ts, REG_CONTROL,
-					REG_SCAN_CYCLE, SCAN_TIME);
-		r69001_set_mode(ts, ts->mode, POLL_INTERVAL);
-	}
+	if (ts->data.mode.mode == INTERRUPT_MODE) {
+		if (mode == SCAN_MODE_STOP) {
+			/* if receive a touchscreen interrupt, but the scan
+			 * mode is stop and we are in interrupt mode, that means
+			 * touch panel just power on, so re-init it
+			 */
+			ts->data.mode.mode = UNKNOW_MODE;
+			r69001_ts_write_data(ts, REG_CONTROL,
+						REG_SCAN_CYCLE, SCAN_TIME);
+			r69001_set_mode(ts, ts->mode, POLL_INTERVAL);
+		}
 
-	r69001_ts_read_coordinates_data(ts);
-	r69001_ts_report_coordinates_data(ts);
+		r69001_ts_read_coordinates_data(ts);
+		r69001_ts_report_coordinates_data(ts);
+	}
 
 	return IRQ_HANDLED;
 }
@@ -287,6 +308,9 @@ static void r69001_set_mode(struct r69001_ts_data *ts, u8 mode, u16 poll_time)
 
 		r69001_ts_write_data(ts, REG_CONTROL,
 				REG_SCAN_MODE, SCAN_MODE_STOP);
+		r69001_ts_write_data(ts, REG_CONTROL,
+				REG_INT_SIGNAL_OUTPUT_CTRL,
+				DEFAULT_INTERRUPT_MASK);
 		msleep(100);
 		r69001_ts_write_data(ts, REG_CONTROL,
 				REG_SCAN_MODE, SCAN_MODE_FULL_SCAN);
@@ -315,12 +339,124 @@ static void r69001_set_mode(struct r69001_ts_data *ts, u8 mode, u16 poll_time)
 			ts->data.mode.poll_time = POLL_INTERVAL;
 		ts->data.mode.mode = mode;
 		break;
+	case CALIBRATION_INTERRUPT_MODE:
+		r69001_ts_write_data(ts, REG_CONTROL,
+				REG_SCAN_MODE, SCAN_MODE_STOP);
+		r69001_ts_write_data(ts, REG_CONTROL,
+				REG_INT_SIGNAL_OUTPUT_CTRL,
+				CALIBRATION_INTERRUPT_MASK);
+		msleep(100);
+		r69001_ts_write_data(ts, REG_CONTROL,
+				REG_SCAN_MODE, SCAN_MODE_CALIBRATION);
+		ts->data.mode.mode = mode;
+		break;
 	default:
 		dev_err(&client->dev, "Set Int Ctl bad parameter = %d\n", mode);
 		break;
 	}
 }
 
+#ifdef CONFIG_DEBUG_FS
+static int r69001_ts_dbgfs_show(struct seq_file *seq, void *unused)
+{
+	struct r69001_ts_data *ts;
+
+	ts = (struct r69001_ts_data *)seq->private;
+	if (!ts)
+		return -EFAULT;
+
+	if (ts->data.mode.mode > MODE_COUNT)
+		return -EFAULT;
+
+	seq_printf(seq, "%s\n", r69001_ts_modes[ts->data.mode.mode]);
+
+	return 0;
+}
+
+static int r69001_ts_dbgfs_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, r69001_ts_dbgfs_show, inode->i_private);
+}
+
+static ssize_t r69001_ts_dbgfs_write(struct file *file,
+		const char __user *user_buf, size_t count, loff_t *ppos)
+{
+	int i;
+	struct seq_file *seq;
+	struct r69001_ts_data *ts;
+	char buf[MODE_STRING_MAX_LEN] = {0};
+
+	if (count > sizeof(buf) - 1)
+		return -EINVAL;
+
+	if (copy_from_user(buf, user_buf, count - 1))
+		return -EFAULT;
+
+	seq = (struct seq_file *)file->private_data;
+	if (!seq) {
+		pr_warn("r69001-touchscreen: Failed to get seq_file\n");
+		return -EFAULT;
+	}
+
+	ts = (struct r69001_ts_data *)seq->private;
+	if (!ts) {
+		pr_warn("r69001-touchscreen: Failed to get private data\n");
+		return -EFAULT;
+	}
+
+	for (i = 0; i < MODE_COUNT; i++) {
+		if (!strncmp(buf, r69001_ts_modes[i], MODE_STRING_MAX_LEN)) {
+			r69001_set_mode(ts, i, POLL_INTERVAL);
+			break;
+		}
+	}
+
+	if (i == MODE_COUNT) {
+		pr_warn("r69001-touchscreen: Invalid mode: %s\n", buf);
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static const struct file_operations r69001_ts_dbgfs_fops = {
+	.owner		= THIS_MODULE,
+	.open		= r69001_ts_dbgfs_open,
+	.read		= seq_read,
+	.write		= r69001_ts_dbgfs_write,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int r69001_ts_create_dbgfs(struct r69001_ts_data *ts)
+{
+	struct dentry *entry;
+
+	r69001_ts_dbgfs_root = debugfs_create_dir(R69001_TS_NAME, NULL);
+	if (!r69001_ts_dbgfs_root) {
+		dev_warn(&ts->client->dev, "debugfs_create_dir failed\n");
+		return -ENOMEM;
+	}
+
+	entry = debugfs_create_file(MODE_FILE_NAME, S_IRUGO,
+			r69001_ts_dbgfs_root,
+			(void *)ts, &r69001_ts_dbgfs_fops);
+	if (!entry) {
+		debugfs_remove_recursive(r69001_ts_dbgfs_root);
+		r69001_ts_dbgfs_root = NULL;
+		dev_warn(&ts->client->dev, "%s debugfs entry creation failed\n",
+				MODE_FILE_NAME);
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
+static void r69001_ts_remove_dbgfs(void)
+{
+	debugfs_remove_recursive(r69001_ts_dbgfs_root);
+}
+#endif
 
 static int
 r69001_ts_probe(struct i2c_client *client, const struct i2c_device_id *id)
@@ -388,6 +524,13 @@ r69001_ts_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		dev_err(&client->dev, "Failed to register interrupt\n");
 		goto err4;
 	}
+
+#ifdef CONFIG_DEBUG_FS
+	error = r69001_ts_create_dbgfs(ts);
+	if (error)
+		dev_warn(&client->dev, "Failed to create debugfs\n");
+#endif
+
 	return 0;
 
 err4:
@@ -403,6 +546,10 @@ err1:
 static int r69001_ts_remove(struct i2c_client *client)
 {
 	struct r69001_ts_data *ts = i2c_get_clientdata(client);
+
+#ifdef CONFIG_DEBUG_FS
+	r69001_ts_remove_dbgfs();
+#endif
 
 	input_unregister_device(ts->input_dev);
 	if (client->irq)
