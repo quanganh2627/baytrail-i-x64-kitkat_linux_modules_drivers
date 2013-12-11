f5390fb2d867a4b3bce1590fc7f985214d89bc1a
Sundar Iyer <sundar.iyer@intel.com>
2013-12-11 16:45:34 +0530

    i2c/dw_i2c: implement arbitration for shared host controller
    BZ: 177468
    
    For platforms where an i2c host is shared between multiple entities like a CPU,
    and a u-controller, we need some synchronization mechanism for i2c access. The
    actual synchronization is platform dependent.
    
    This patch adds the support to introduce sync for such hosts. Also, we disable
    the power management (also runtime) for such hosts to avoid lockup issues. We
    currently use the flag "shared_host" to signify a shared host and implement the
    synchronization.
    
    TODO: Update the driver for UEFI changes
    
    Change-Id: I740c3a446fdb42690c964ca057b77d533a3f9683
    Signed-off-by: Sundar Iyer <sundar.iyer@intel.com>
    Signed-off-by: Souvik Kumar Chakravarty <souvik.k.chakravarty@intel.com>
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>
    Signed-off-by: Bin Yang <bin.yang@intel.com>

--- i2c/busses/i2c-designware-platdrv.c
+++ i2c/busses/i2c-designware-platdrv.c
@@ -161,15 +161,15 @@ static int __init dw_i2c_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	platform_set_drvdata(pdev, dev);
-
-	acpi_i2c_register_devices(&dev->adapter);
-
-	pm_runtime_set_active(&pdev->dev);
-	pm_runtime_enable(&pdev->dev);
-	pm_runtime_allow(&pdev->dev);
-	pm_runtime_use_autosuspend(&pdev->dev);
-	pm_runtime_set_autosuspend_delay(&pdev->dev, 50);
+	if (dev->shared_host) {
+		pm_runtime_forbid(&pdev->dev);
+	} else {
+		pm_runtime_set_active(&pdev->dev);
+		pm_runtime_enable(&pdev->dev);
+		pm_runtime_allow(&pdev->dev);
+		pm_runtime_use_autosuspend(&pdev->dev);
+		pm_runtime_set_autosuspend_delay(&pdev->dev, 50);
+	}
 
 	return 0;
 }
