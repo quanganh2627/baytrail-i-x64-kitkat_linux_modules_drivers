be15331c3dfcc0bb9ab0477e21a8f7576950db46
Flavien Boutoille <flavienx.boutoille@intel.com>
2014-02-19 16:13:36 +0100

    tp2e: Add a probe for recoverable fabric errors
    BZ: 174709
    
    Recoverable fabric errors, when caught on an IRQ, can be sent
    through tp2e, using a dedicated event (similar to generic_event).
    This allows to activate/deactivate recoverable fabric error events
    independently from generic events.
    
    Change-Id: I206656bedb617c6ebaa5688bfa5bcd1575eee5d3
    Signed-off-by: Flavien Boutoille <flavienx.boutoille@intel.com>

--- misc/tp2e/tp2e_probe_generic_event.h
+++ misc/tp2e/tp2e_probe_generic_event.h
@@ -8,12 +8,14 @@
  */
 #define TRACE_SYSTEM tp2e
 #define TP2E_GENERIC_EVENT_NAME tp2e_generic_event
+#define TP2E_SCU_RECOV_EVENT_NAME tp2e_scu_recov_event
 
 static void tp2e_probe_generic_event(void *cb_data,
 				     enum tp2e_ev_type tp2e_ev_type,
 				     char *submitter_name, char *ev_name,
 				     char *data0, char *data1, char *data2,
-				     char *data3, char *data4, char *data5)
+				     char *data3, char *data4, char *data5,
+				     char *filelist)
 {
 	enum ct_ev_type ev_type;
 
@@ -34,9 +36,12 @@ static void tp2e_probe_generic_event(void *cb_data,
 		ev_type = CT_EV_INFO;
 		break;
 	}
-	kct_log(ev_type, submitter_name, ev_name, 0, data0, data1, data2, data3, data4, data5);
+	kct_log(ev_type, submitter_name, ev_name, 0, data0, data1, data2,
+		data3, data4, data5, filelist);
 }
 
 #endif /* _TP2E_PROBE_GENERIC_EVENT_H_ */
 
 DEFINE_PROBE(TP2E_GENERIC_EVENT_NAME, tp2e_probe_generic_event);
+
+DEFINE_PROBE(TP2E_SCU_RECOV_EVENT_NAME, tp2e_probe_generic_event);
