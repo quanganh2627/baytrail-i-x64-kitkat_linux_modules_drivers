5df9be4b52dc359e453ff4cbdc266b9e13e0e9d5
Michel Jauffres <michelx.jauffres@intel.com>
2014-06-10 18:11:53 +0200

    Clarify reboot option to MOS when exiting from ROS
    BZ: 202863
    
    When exiting from ROS we should go to the appropriate mode,
    depending on the entered adb command.
    If the command is 'adb reboot' or 'adb reboot bootloader',
    then OSIP must be restored and the device should reboot
    to MOS.
    If the command is 'adb reboot recovery', then OSIP is
    invalidated, and the device will reboot again to ROS.
    
    Change-Id: I77df6ee1416e717559f9c423cbb827d29f685a50
    Signed-off-by: Michel Jauffres <michelx.jauffres@intel.com>

--- platform/x86/intel_mid_osip.c
+++ platform/x86/intel_mid_osip.c
@@ -328,7 +328,9 @@ static int osip_reboot_target_call(const char *target, int id)
 	if (ret_ipc < 0)
 		pr_err("%s cannot write %s reboot reason in OSNIB\n",
 			__func__, target);
-	if (id == SIGNED_MOS_ATTR) {
+	if (id == SIGNED_MOS_ATTR || id == SIGNED_POS_ATTR) {
+		/* If device is already in RECOVERY we must be able */
+		/* to reboot in MOS if given target is MOS or POS.  */
 		pr_warn("[REBOOT] %s, restoring OSIP\n", __func__);
 		access_osip_record(osip_restore, (void *)0);
 	}
