3524d236f4a35efe588b4502788c17574ae811bb
R, Kannappan <r.kannappan@intel.com>
2014-06-24 20:18:06 +0530

    EM/ADC: Add Platform data for Whiskeycove ADC support
    BZ: 213441
    
    This patch adds the platform data for the ADC channels
    supported by Whiskeycove, which shall be used by the
    GPADC driver
    
    Change-Id: Ia94728a00e09b544c95fbaa3fe4e8c3aa69b5d50
    Signed-off-by: R, Kannappan <r.kannappan@intel.com>

--- mfd/intel_pmic/whiskey_cove.c
+++ mfd/intel_pmic/whiskey_cove.c
@@ -27,6 +27,11 @@
 #include "./pmic.h"
 #include <linux/regulator/intel_whiskey_cove_pmic.h>
 
+#include <asm/intel_basincove_gpadc.h>
+#include <linux/iio/iio.h>
+#include <linux/iio/machine.h>
+#include <linux/iio/types.h>
+
 #define WHISKEY_COVE_IRQ_NUM	17
 
 #define CHIPID		0x00
@@ -48,6 +53,80 @@
 #define MTHRMIRQ3	0xDA
 #define MCHGRIRQ	0x17
 
+static struct gpadc_regmap_t whiskeycove_gpadc_regmaps[GPADC_NUM_CHANNELS] = {
+	{"VBAT",	5,	0x4F03, 0x4F04, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATID",	4,	0x4F06, 0x4F07, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"PMICTEMP",	3,	0x4F42,	0x4F43, 0x4F33, 0x4F34, 0x4F33, 0x4F34},
+	{"BATTEMP0",	2,	0x4F15, 0x4F16, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATTEMP1",	2,	0x4F17, 0x4F18, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"SYSTEMP0",	3,	0x4F38, 0x4F39, 0x4F23, 0x4F24, 0x4F25, 0x4F26},
+	{"SYSTEMP1",	3,	0x4F3A, 0x4F3B, 0x4F27, 0x4F28, 0x4F29, 0x4F2A},
+	{"SYSTEMP2",	3,	0x4F3C, 0x4F3D, 0x4F2B, 0x4F2C, 0x4F2D, 0x4F2E},
+	{"USBID",	1,	0x4F08, 0x4F09, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"PEAK",	7,	0x4F13, 0x4F14, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"AGND",	6,	0x4F0A, 0x4F0B, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"VREF",	6,	0x4F0A, 0x4F0B, 0xFF, 0xFF, 0xFF, 0xFF},
+};
+
+static struct gpadc_regs_t whiskeycove_gpadc_regs = {
+	.gpadcreq	=	0x4F02,
+	.gpadcreq_irqen	=	0,
+	.gpadcreq_busy	=	(1 << 0),
+	.mirqlvl1	=	0x6e0E,
+	.mirqlvl1_adc	=	(1 << 3),
+	.adc1cntl	=	0x4F05,
+	.adcirq		=	0x6E08,
+	.madcirq	=	0x6E15,
+};
+
+#define MSIC_ADC_MAP(_adc_channel_label,			\
+		     _consumer_dev_name,                        \
+		     _consumer_channel)                         \
+	{                                                       \
+		.adc_channel_label = _adc_channel_label,        \
+		.consumer_dev_name = _consumer_dev_name,        \
+		.consumer_channel = _consumer_channel,          \
+	}
+
+static struct iio_map wc_iio_maps[] = {
+	MSIC_ADC_MAP("CH0", "VIBAT", "VBAT"),
+	MSIC_ADC_MAP("CH1", "BATID", "BATID"),
+	MSIC_ADC_MAP("CH2", "PMICTEMP", "PMICTEMP"),
+	MSIC_ADC_MAP("CH3", "BATTEMP", "BATTEMP0"),
+	MSIC_ADC_MAP("CH4", "BATTEMP", "BATTEMP1"),
+	MSIC_ADC_MAP("CH5", "SYSTEMP", "SYSTEMP0"),
+	MSIC_ADC_MAP("CH6", "SYSTEMP", "SYSTEMP1"),
+	MSIC_ADC_MAP("CH7", "SYSTEMP", "SYSTEMP2"),
+	MSIC_ADC_MAP("CH8", "USBID", "USBID"),
+	MSIC_ADC_MAP("CH9", "PEAK", "PEAK"),
+	MSIC_ADC_MAP("CH10", "GPMEAS", "AGND"),
+	MSIC_ADC_MAP("CH11", "GPMEAS", "VREF"),
+	{ },
+};
+
+#define MSIC_ADC_CHANNEL(_type, _channel, _datasheet_name) \
+	{                               \
+		.indexed = 1,           \
+		.type = _type,          \
+		.channel = _channel,    \
+		.datasheet_name = _datasheet_name,      \
+	}
+
+static const struct iio_chan_spec const wc_adc_channels[] = {
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 0, "CH0"),
+	MSIC_ADC_CHANNEL(IIO_RESISTANCE, 1, "CH1"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 2, "CH2"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 3, "CH3"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 4, "CH4"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 5, "CH5"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 6, "CH6"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 7, "CH7"),
+	MSIC_ADC_CHANNEL(IIO_RESISTANCE, 8, "CH8"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 9, "CH9"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 10, "CH10"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 11, "CH11"),
+};
+
 enum {
 	PWRSRC_LVL1 = 0,
 	THRM_LVL1,
@@ -68,6 +147,7 @@ enum {
 };
 
 struct intel_mid_pmic whiskey_cove_pmic;
+static struct intel_basincove_gpadc_platform_data wc_adc_pdata;
 
 static struct resource gpio_resources[] = {
 	{
@@ -285,10 +365,26 @@ struct intel_pmic_irqregmap whiskey_cove_irqregmap[] = {
 	},
 };
 
+static void wc_set_adc_pdata(void)
+{
+	wc_adc_pdata.channel_num = GPADC_NUM_CHANNELS;
+	wc_adc_pdata.intr_mask = MUSBID | MPEAK | MBATTEMP
+		| MSYSTEMP | MBATT | MVIBATT | MGPMEAS | MCCTICK;
+	wc_adc_pdata.gpadc_iio_maps = wc_iio_maps;
+	wc_adc_pdata.gpadc_regmaps = whiskeycove_gpadc_regmaps;
+	wc_adc_pdata.gpadc_regs = &whiskeycove_gpadc_regs;
+	wc_adc_pdata.gpadc_channels = wc_adc_channels;
+
+	intel_mid_pmic_set_pdata("whiskey_cove_adc", (void *)&wc_adc_pdata,
+			sizeof(struct intel_basincove_gpadc_platform_data), 0);
+}
+
 static int whiskey_cove_init(void)
 {
 	pr_info("Whiskey Cove: ID 0x%02X, VERSION 0x%02X\n",
 		intel_mid_pmic_readb(CHIPID), intel_mid_pmic_readb(CHIPVER));
+
+	wc_set_adc_pdata();
 	return 0;
 }
 
